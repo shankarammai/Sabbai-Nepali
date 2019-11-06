7c3a6ecaabbc389e34dbe86251c8707dfa70e4dc
Shankar Ammai <ammai.shankar@gmail.com>
2019-11-06 14:27:36 +0000

    Initial commit

---
+++ data.js
@@ -0,0 +1,263 @@
+var allnepalistations=[
+{
+	url: "http://kalika-stream.softnep.com:7740/;stream.nsv&type=mp3",
+    name: "Kalika FM 95.2 MHz",
+	image:"http://radio.nepal.fm/wp-content/uploads/kalikafm.jpg"
+},
+  
+
+{
+    url: "http://kantipur-stream.softnep.com:7248/;stream.nsv&type=mp3",
+    name: "Radio Kantipur",
+    image: "http://radio.nepal.fm/wp-content/uploads/radio02.jpg"
+},
+
+{
+    url: "http://ujyaalo-stream.softnep.com:7710/;stream.nsv&amp;type=mp3&amp;volume=50",
+    name: "Ujyaalo 90 Network ",
+    image: "http://radio.nepal.fm/wp-content/uploads/ujyaalo-150x137.jpg"
+},
+{
+    url:"http://streaming.hamropatro.com:8004/;stream.nsv&amp;type=mp3&amp;volume=50",
+    name:"Sagarmatha FM",
+    image:""
+
+},
+{
+	url:"http://streaming.hamropatro.com:8631/;stream.mp3",
+	name:"Image FM",
+	image:""
+},
+
+{
+	url: "http://stream.zenolive.com/2w81t82wx3duv",
+    name: "Radio Audio ",
+	image:"https://cdn-profiles.tunein.com/s105813/images/logoq.png"
+},
+{
+	url:"http://streaming.hamropatro.com:8399/;stream.nsv&amp;type=mp3&amp;volume=50",
+    name:"Synergy FM",
+    image:""
+},
+{
+    url:"http://ufm.online.org.np:5428/;stream.nsv&amp;type=mp3&amp;volume=50",
+    name:"Radio Upatyaka",
+	image:""
+},
+{
+	url: "http://streaming.softnep.net:7025/;stream.nsv&type=mp3",
+    name: "Radio Thaha Sanchar Hetauda",
+    image: "http://radio.nepal.fm/wp-content/uploads/radio08.jpg"
+},
+{
+    url: "http://streaming.softnep.net:8037/;stream.nsv&type=mp3",
+    name: "Capital FM",
+	image:""
+},
+{
+    url: "http://stream.zenolive.com/2w81t82wx3duv",
+    name: "Radio Audio ",
+	image:""
+},
+{
+    url: "http://streaming.softnep.net:8057/;stream.nsv&type=mp3&autostart=true",
+    name: "Radio Makwanpur Hetauda",
+	image:""
+},
+{
+    url: "http://live.itechnepal.com:8541/stream",
+    name: "Radio Himalaya",
+	image:""
+},
+{
+    url:"http://streaming.softnep.net:8053/;stream.nsv&amp;type=mp3&amp;volume=50",
+    name:"Radio Miteri",
+	image:""
+    
+},
+{
+    url: "http://streaming.softnep.net:8031/;stream.nsv&type=mp3&autostart=true",
+    name: "Radio Triveni",
+	image:""
+  
+},
+{
+    url: "http://streaming.softnep.net:8017/;stream.nsv&type=mp3",
+    name: "Radio Birgunj",
+	image:""
+    
+},
+{
+    url: "http://167.114.64.181:8220/;",
+    name: "Look Geet Radio",
+	image:""
+},
+{
+    url: "http://198.27.83.198:5098/stream",
+    name: "Hits FM 91.2 MHz",
+	image:""
+},
+{
+    url: "http://streaming.softnep.net:8007/;stream.nsv&type=mp3",
+    name: "Radio Seti",
+	image:""
+},
+{
+    url: "http://streaming.softnep.net:8015/;stream.nsv&type=mp3",
+    name: "Radio 24 Times",
+	image:""
+},    
+{
+    url: "http://bbcwssc.ic.llnwd.net/stream/bbcwssc_mp1_ws-nepla_backup",
+    name: "BBC Nepal",
+	image:""
+},
+{
+    url: "http://live.itechnepal.com:8288/stream?type=.mp3",
+    name: "Navajyoti FM ",
+	image:""
+},
+{
+    url: "http://edge-audio-02-gos1.sharp-stream.com/ssvcbfbs3.mp3",
+    name: "BFBS Gurkha Radio",
+	image:""
+},
+{
+    url:"http://192.240.102.133:11454/;stream.nsv&amp;type=mp3&amp;volume=50",
+    name:"Bollywood Evergreeen",
+	image:""
+},
+{
+	url:"http://live.itechnepal.com:8530/live",
+	name:"Classic FM 101.2 MHz",
+	image:""
+},
+{
+    url: "http://streaming.softnep.com:8085/;stream?type=http&nocache=288550",
+    name: "Radio Lumbini",
+    image: "http://radio.nepal.fm/wp-content/uploads/Radio-Lumbini.jpg"
+},
+{
+    url: "http://streaming.softnep.net:8003/stream?type=http&nocache=288550",
+    name: "Butwal FM",
+	image:""
+},
+{
+    url:"http://live.itechnepal.com:9449/stream?type=.mp3;stream.nsv&amp;type=mp3&amp;volume=50",
+    name:"Radio Dhading",
+    image:""
+
+},
+{
+    url:"http://live.itechnepal.com:9210/stream?type=.mp3/;stream.nsv&amp;type=mp3&amp;volume=50",
+    name:"Radio Tulsipur",
+    image:""
+},
+{
+    url: "http://streaming.softnep.net:8141/;stream.nsv&type=mp3",
+    name: "Radio Myagdi Kali",
+	image:""
+},
+{
+    url: "http://streaming.softnep.net:8143/;stream.nsv&type=mp3",
+    name: "Radio Bhojpuriya FM",
+	image:""
+},
+{
+    url: "http://live.itechnepal.com:7042/stream?type=.mp3",
+    name: "Dautari FM",
+	image:""
+},
+{
+    url: "http://50.7.99.163:16118/stream",
+    name: "Big FM 101.2 MHZ",
+	image:""
+},
+{
+    url: "http://streaming.softnep.net:8101/;stream.nsv&type=mp3",
+    name: "Radio Karnali",
+	image:""
+},
+{
+    url: "http://streaming.softnep.net:8069/;stream.nsv&type=mp3",
+    name: "Radio Devghat ",
+	image:""
+},
+{
+    url: "http://streaming.hamropatro.com:8383/;",
+    name: "Vijaya FM",
+	image:""
+},
+{
+    url: "http://streaming.softnep.net:8063/;stream.nsv&type=mp3",
+    name: "Afno FM Rukum ",
+	image:""
+},
+{
+    url: "http://live.itechnepal.com:8380/stream",
+    name: "Bulbul FM",
+	image:""
+},
+{
+    url: "http://streaming.softnep.net:8059/;stream.nsv&type=mp3",
+    name: "KTM 90.3 FM",
+	image:""
+},
+{
+    url: "http://live.itechnepal.com:7031/stream",
+    name: "Nagrik FM 107.5 MHz",
+	image:""
+},
+{
+    url: "http://streaming.softnep.net:8051/;stream.nsv&type=mp3",
+    name: "Radio Melamchi",
+	image:""
+},
+{
+    url: "http://streaming.softnep.net:8041/;stream.nsv&type=mp3",
+    name: "Radio Sindhu",
+	image:""
+},
+{
+    url: "http://37.59.42.207:8166/;stream.mp3",
+    name: "Radio Pariwartan",
+	image:""
+},
+{
+    url: "http://streaming.softnep.net:8101/;stream.nsv&type=mp3",
+    name: "Radio Karnali Jumla",
+    image: "http://radio.nepal.fm/wp-content/uploads/radiokarnali.jpg"
+},
+{
+    url: "http://streaming.softnep.net:8093/;stream.nsv&type=mp3",
+    name: "Hamro Cricket",
+    image: "http://radio.nepal.fm/wp-content/uploads/hamrocricket.jpg"
+},
+{
+    url: "http://live.itechnepal.com:7014/stream",
+    name: "Namaste Fm 107.2 MHz ",
+	image:""
+},
+{
+    url: "http://live.itech.host:9150/stream",
+    name: "Radio Terhathum",
+	image:""
+},
+{
+    url: "http://live.itech.host:9479/stream",
+    name: "Vijayapur FM",
+	image:""
+},
+{
+    url: "http://live.itech.host:8021/stream",
+    name: "Pradesh FM 105 Mhz ",
+	image:""
+},
+{
+	url:"http://streaming.hamropatro.com:8244/",
+	name:"Radio Mithila",
+	image:""
+		
+	}
+
+];
